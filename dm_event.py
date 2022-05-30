627a9a358f27f7bbf6592b690712ed7c26410c1c
Agustin Lantero Barreda <agustin.lantero@alumnos.unican.es>
2022-05-30 19:53:02 +0200

    Added fixed parameters. Added upper limits for background and signal+background. Added verbose plots.

--- dm_event.py
+++ dm_event.py
@@ -101,6 +101,8 @@ class dm_event(object):
         ### Events in Eee
         self.bkg_ev = np.random.poisson(self.lamb, self.npix)
 
+
+    """
     def joint_probability(self, **kwargs):
 
         x0 = kwargs["x0"] if "x0" in kwargs else [self.npix/0.1, 0, 1.6, 1, 1e-4, np.log10(self.cross_section)]
@@ -163,7 +165,7 @@ class dm_event(object):
         c = r.TCanvas()
         hist.Draw()
         c.SetLogy()
-
+    """
 
     def likelihood(self, **kwargs):
         """ Minimizes the log likelihood of signal+background model.
@@ -180,36 +182,42 @@ class dm_event(object):
         self.cross_section_original = self.cross_section
 
         def prob(p,x):
-            Npix,mu,noise,gain,dc,xs = p[0],p[1],p[2],p[3],p[4],p[5]
-            #mu,noise,gain,dc,xs = p[0],p[1],p[2],p[3]*self.t_exp,p[4]
+            Npix,mu,noise,gain,dc,xs = p[0],p[1],p[2],p[3],p[4]*self.t_exp,p[5]
             #print(Npix,mu,noise,gain,dc,xs)
             self.cross_section = 10**xs
             self.normalization_signal()
+ 
+            #f_str_num = []
+            #f_str = []
             
-            #S = np.array([1-np.sum(self.fs)] + self.fs.tolist())
+            ### Adds the 0 electron to peak to DM interaction probability 
             S = np.array([self.npix-self.s] + (self.fs*self.s).tolist())/self.npix
-            pdf = 0 #Npix*S[0]*poisson.pmf(0,dc)*norm.pdf(x,mu*gain,noise*gain)
-            #nPeaks = np.floor(np.max(self.events)+1).astype(int)
-            f_str_num = []
-            f_str = []
+            pdf = 0 
+            
             for ntot in range(0,self.nPeaks):
                 for j in range(0,ntot+1):
-                    f_str_num.append("Npix*S({})*P({})*G({})".format(np.round(S[j],2),np.round(poisson.pmf(ntot-j,dc),2),ntot))
-
-                    f_str.append("Npix*S({})*P({})*G({})".format(j,ntot-j,ntot))
+                    #f_str_num.append("Npix*S({})*P({})*G({})".format(np.round(S[j],2),np.round(poisson.pmf(ntot-j,dc),2),ntot))
+                    #f_str.append("Npix*S({})*P({})*G({})".format(j,ntot-j,ntot))
                     pdf += Npix*S[j]*poisson.pmf(ntot-j,dc)*norm.pdf(x,(mu+ntot)*gain,noise*gain)
             #print("+".join(f_str))
             #print("+".join(f_str_num))
             return pdf
 
-        if upper_limit:
-            dc, deltaLL = upper_limit
-            self.simul_bkg(dc)
-            self.events = self.bkg_ev
+
+        if upper_limit and simulate:
+            deltaLL, bkg_only = upper_limit
+            dc = simulate 
+            if bkg_only:
+                self.simul_bkg(dc)
+                self.events = self.bkg_ev
+                self.events = self.events + np.random.normal(0,self.noise, self.npix)
+                self.events = self.events.tolist()
+            else:
+                self.simul_ev(dc)
             ### Add readout noise
-            self.events = self.events + np.random.normal(0,self.noise, self.npix)
-            self.events = self.events.tolist()
-        elif simulate:
+        if upper_limit and not simulate:
+            deltaLL = upper_limit
+        elif simulate and not upper_limit:
             dc = simulate 
             self.simul_ev(dc)
         elif not simulate and not upper_limit:
@@ -218,35 +226,65 @@ class dm_event(object):
             else:
                 print("Data Not yet simulated. Goodbye!!")
 
+
+        ### Sets the number of electron peaks to fit
         self.nPeaks = kwargs["nPeaks"] if "nPeaks" in kwargs else int(np.round(np.max(self.events)))
         print("Number of electron peaks: ", self.nPeaks)
-        #self.nPeaks = 5
+
+        ### Sets the number of bins and the bin content
         bin_size = kwargs["bin_size"] if "bin_size" in kwargs else 0.1
         nbins = int((self.xmax-self.xmin)/bin_size)
+
         ### Creates and histogram to get the bin content
-        hist = plt.hist(self.events, bins=nbins)
+        hist = np.histogram(self.events, nbins)
         n_data, dx = hist[0],hist[1]
 
-        """
         if verbose:
+            ### Plot the events histogram (Useful to see the number of peaks)
             dx_m = (dx[:-1] + dx[1:])/2
             dx_m = dx[1:]#.flip(dx)))
             n_theo = prob(x0,dx_m)*np.diff(dx)
-            plt.plot(dx_m, n_theo, color = "r", label = "Initial Guess")
+            #plt.plot(dx_m, n_theo, color = "r", label = "Initial Guess")
+            plt.hist(self.events, nbins)
             plt.yscale("log")
             plt.ylim(0.1,None)
-        """
- 
+            plt.show() 
+            plt.clf()
+
         def log_like(theta,n_data,dx):
             ### Uses the bin center approximation for binning the likelihood
             dx_m = (dx[:-1] + dx[1:])/2
             n_theo = prob(theta,dx_m)*np.diff(dx)
-            #lnL = -np.log(scipy.special.factorial(n)) -np.sum(n_data*np.log(n_theo)) + np.sum(scipy.special.factorial(n_data))
-            lnL = theta[0] - np.sum(n_data*np.log(n_theo)) 
+            #lnL = theta[0] - np.sum(n_data*np.log(n_theo)) 
+            lnL = np.sum(n_theo) - np.sum(n_data*np.log(n_theo)) 
             return lnL
 
         ### Minimizes the loglikelihood
-        theta_max = minimize(log_like, x0, bounds=pars_lims,method='Powell', args = (n_data,dx)).x
+        if len(fix_pars) != 0:
+            ### Fix the given parameter numbers
+            p = np.zeros_like(x0)+np.inf
+            x0 = np.array(x0)
+            p[fix_pars] = x0[fix_pars]
+            not_fix_pars = np.where(x0!=p)[0]
+            pars_lims_nofix = np.array(pars_lims)[not_fix_pars]
+            x0_nofix = x0[not_fix_pars]
+
+            def log_like_fix(theta, n_data, dx):
+                """ Function with fixed parameters
+                """
+                p[not_fix_pars] = theta
+                return log_like(p, n_data, dx)
+
+            ### Minimizes log-likelihood
+            theta_max = minimize(log_like_fix, x0_nofix, bounds=pars_lims_nofix,method='Powell', args = (n_data,dx)).x
+
+            ### Returns fitted+fixed parameters
+            p[not_fix_pars] = theta_max
+            theta_max = p
+        else:
+            ### Not fixed parameters
+            theta_max = minimize(log_like, x0_nofix, bounds=pars_lims,method='Powell', args = (n_data,dx)).x
+
         print("----------- Fit Result -----------")
         pars_name = [r"$Norm$", r"$\mu_{0}$", r"$noise$", r"$\Omega$",r"$\lambda$", r"$\sigma_{DM-e}$"]
         fit_leg = []
@@ -254,8 +292,8 @@ class dm_event(object):
             fit_leg.append(name+" = "+str(p))
         textstr = "\n".join(fit_leg)
         print(textstr) 
+        print("----------------------------------")
 
-        lnL_bkg_only = log_like(theta_max, n_data, dx)
 
         if verbose:
             ### Plot fit and histogram
@@ -293,21 +331,34 @@ class dm_event(object):
 
         ### Calculates the upper limit
         if upper_limit:
+            ### Value of the likelihood at its maximum
+            lnL_max = -log_like(theta_max, n_data, dx)
+
             def lnL_dLL(xs_range, n_data, dx):
                 """ Function to find the value where ln_max- ln_confidence = deltaLL
                 """
                 theta = np.array(theta_max[:-1]).tolist() + [xs_range]
-                return -log_like(theta, n_data, dx)-lnL_bkg_only+deltaLL
-            self.cross_section_95 = 10**bisect(lnL_dLL, pars_lims[-1][0], pars_lims[-1][1], args=(n_data,dx),rtol=1e-3)
-            print(self.cross_section_95)
+                return -log_like(theta, n_data, dx)-lnL_max+deltaLL
+            self.cross_section_dLL = 10**bisect(lnL_dLL, pars_lims[-1][0], pars_lims[-1][1], args=(n_data,dx),rtol=1e-3)
+            print(self.cross_section_dLL)
 
+            if verbose:
+                xs_range = np.linspace(pars_lims[-1][0], pars_lims[-1][1], 100)
+                dLL_range = [lnL_dLL(xs_i,n_data,dx) for xs_i in xs_range]
+                plt.plot(10**xs_range, dLL_range)
+                plt.xscale("log")
+                plt.ylim(0,deltaLL*1.5)
+                plt.ylabel(r"$d\mathcal{LL}$")
+                plt.xlabel(r"$\sigma$ [cm$^{-2}$]")
+                plt.show()
+ 
 
     def verbose(self):
         print("------------ Using {} model -----------".format(self.dRdE_name))
         print("Number of Total Pixels: ", self.npix)
         print("Number of Signal events: ", self.n_s_det)
         if hasattr(self, "theta"): print("Likelihood parameters: ", self.theta)
-        if hasattr(self, "cross_section_95"): print("Upper Limit Cross Section: ", self.cross_section_95)
+        if hasattr(self, "cross_section_dLL"): print("Upper Limit Cross Section: ", self.cross_section_dLL)
         if hasattr(self, "theta_confidence"): print("Theta Confidence intervals: ", self.confidence)
 
     def plot_var(self,var_name, step = 100, **kwargs):
